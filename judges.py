bd26bf6a2dd885aec4cd1791c0c111f452cba56a
Andrei Lupu <alupu@fb.com>
2024-05-20 00:02:36 +0100

    Remove GPT references

--- judges.py
+++ judges.py
@@ -2,7 +2,7 @@ from fastchat.model import get_conversation_template
 import re
 from system_prompts import get_judge_system_prompt
 
-from language_models import GPT
+# from language_models import GPT
 
 
 def load_judge(args):
@@ -50,27 +50,27 @@ class NoJudge(JudgeBase):
         return [1 for _ in prompt_list]
 
 
-class GPTJudge(JudgeBase):
-    def __init__(self, args):
-        super(GPTJudge, self).__init__(args)
-        self.judge_model = GPT(model_name=self.judge_name)
-
-    def create_conv(self, full_prompt):
-        conv = get_conversation_template(self.judge_name)
-        conv.set_system_message(self.system_prompt)
-        conv.append_message(conv.roles[0], full_prompt)
-        return conv.to_openai_api_messages()
-
-    def score(self, attack_prompt_list, target_response_list):
-        convs_list = [
-            self.create_conv(self.get_judge_prompt(prompt, response))
-            for prompt, response in zip(attack_prompt_list, target_response_list)
-        ]
-        raw_outputs = self.judge_model.batched_generate(
-            convs_list, max_n_tokens=self.max_n_tokens, temperature=self.temperature
-        )
-        outputs = [self.process_output(raw_output) for raw_output in raw_outputs]
-        return outputs
+# class GPTJudge(JudgeBase):
+#     def __init__(self, args):
+#         super(GPTJudge, self).__init__(args)
+#         self.judge_model = GPT(model_name=self.judge_name)
+#
+#     def create_conv(self, full_prompt):
+#         conv = get_conversation_template(self.judge_name)
+#         conv.set_system_message(self.system_prompt)
+#         conv.append_message(conv.roles[0], full_prompt)
+#         return conv.to_openai_api_messages()
+#
+#     def score(self, attack_prompt_list, target_response_list):
+#         convs_list = [
+#             self.create_conv(self.get_judge_prompt(prompt, response))
+#             for prompt, response in zip(attack_prompt_list, target_response_list)
+#         ]
+#         raw_outputs = self.judge_model.batched_generate(
+#             convs_list, max_n_tokens=self.max_n_tokens, temperature=self.temperature
+#         )
+#         outputs = [self.process_output(raw_output) for raw_output in raw_outputs]
+#         return outputs
 
 
 class CeleryJudge(JudgeBase):
